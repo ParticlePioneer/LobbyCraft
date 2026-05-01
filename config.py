b3f36bffbbbe7c65a7bf3d5445b7626b71492540
ParticlePioneer <kalammuneeb@gmail.com>
2026-05-01 12:53:39 +0500

    Configuration variables

---
+++ config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings 
+ 
+class Settings(BaseSettings): 
+    oracle_user:     str 
+    oracle_password: str 
+    oracle_dsn:      str 
+    api_host:        str = '0.0.0.0' 
+    api_port:        int = 8000 
+ 
+    class Config: 
+        env_file = '.env' 
+ 
+settings = Settings() 
